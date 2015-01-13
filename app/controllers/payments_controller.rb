bcff70356a9dfad4cf87a735f4b78f99120df467
Mason Fischer <mason@thoughtbot.com>
2015-01-12 23:32:11 -0600

    Restructure transaction params

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,9 +1,10 @@
 class PaymentsController < ApplicationController
   def create
+    transaction_params = JSON.parse(params.first[0])
     order = Order.find(params[:order_id])
 
-    if params[:key] == ENV['PAYMENT_SECRET_KEY'] && params[:amount] >= 0.02
-        order.pay(payment_transaction: params[:transaction][:id] )
+    if params[:key] == ENV['PAYMENT_SECRET_KEY'] && transaction_params["amount"] >= 0.02
+        order.pay(payment_transaction: transaction_params["transaction"]["hash"] )
     end
 
     head status: :ok
